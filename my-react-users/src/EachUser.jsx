81abf4f23014671b0e1ca80001b09153c970fde3
OdrinaHelen <odrinahelen@gmail.com>
2023-12-22 11:50:31 +0100

    users

---
+++ my-react-users/src/EachUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './eachUser.module.css'
+
+const EachUser = ({ user }) => {
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
+
+export default EachUser;
